a3b97c9e7085aaa2b66e4cb1a9d468bb0051fa34
kandulaakhila <akhilakandula735@gmail.com>
2025-11-01 23:17:28 +0530

    Time: 30 ms (6.56%) | Memory: 148.1 MB (5.52%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -8,25 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- //better solution
+ //optimal solution
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head){
-        Set<Integer>set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
-            ListNode dummy=new ListNode(0);
-            dummy.next=head;
-            ListNode prev=dummy,curr=head;
-            while(curr!=null){
-                if(set.contains(curr.val)){
-                    prev.next=curr.next;
-                }
-                else{
-                    prev=curr;
-                }
-                curr=curr.next;
+        // Step 1: Sort the nums array for binary search
+        Arrays.sort(nums);
+
+        // Step 2: Create a dummy node to simplify edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy, curr = head;
+
+        // Step 3: Traverse the linked list
+        while (curr != null) {
+            // Step 4: Use binary search to check if curr.val is in nums
+            if (Arrays.binarySearch(nums, curr.val) >= 0) {
+                // If found, remove the node
+                prev.next = curr.next;
+            } else {
+                // If not found, move prev forward
+                prev = curr;
             }
-            return dummy.next;
+            // Always move curr forward
+            curr = curr.next;
         }
+
+        // Step 5: Return the new head (after dummy)
+        return dummy.next;
     }
+} 
