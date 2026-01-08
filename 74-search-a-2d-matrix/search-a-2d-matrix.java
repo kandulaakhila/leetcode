f218f38ccb5708251d7eb7474e527f86331f97b1
kandulaakhila <akhilakandula735@gmail.com>
2026-01-08 23:08:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (80.31%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       //brute forces solution
-       //t.c:o(m.n);
-       //s.c:o(1);
-       for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == target) {
-                    return true;
-                }
-            }
+        //optimal solution
+        //t.c:o(log(m.n));
+        //s.c:o(1);
+        int m=matrix.length;//no.of rows
+        int n=matrix[0].length;//no.of columns
+        int left=0,right=m*n-1;//B.S over flattened array
+        while(left<=right){
+            int mid=left+(right-left)/2;//safe mid calculation
+            int row=mid/n;
+            int column=mid%n;
+            int midval=matrix[row][column];//element at midposition
+            if(midval==target) return true;
+            else if(midval<target) left=mid+1;//search right half
+            else right=mid-1;//search left half
         }
         return false;
     }
 }
+        
