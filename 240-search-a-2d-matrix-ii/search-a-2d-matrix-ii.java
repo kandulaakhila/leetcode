09f9247eb930a441e75030d936c0cb5e810cd258
kandulaakhila <akhilakandula735@gmail.com>
2026-01-09 00:29:28 +0530

    Time: 3 ms (86.36%) | Memory: 48.1 MB (68.23%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //better solution
-        //t.c:o(m.logn);
-        //s.c:o(1);
-       // each row is sorted we can B.S each row
-       int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            int left = 0, right = n - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (matrix[i][mid] == target) return true;
-                else if (matrix[i][mid] < target) left = mid + 1;
-                else right = mid - 1;
-            }
-        }
-        return false;
+       //optimal solution
+       //t.c:o(m+n);worst case you traverse one full row and one full column
+       //s.c:o(1);
+       int m=matrix.length;
+       int n=matrix[0].length;
+       int row=0,column=n-1;//starts at top right
+       while(row<m && column>=0 ){
+        int val=matrix[row][column];
+        if(val==target)return true;
+        else if(val>target) column--;//move left
+        else row++;//move down
+       }
+       return false;
     }
 }
