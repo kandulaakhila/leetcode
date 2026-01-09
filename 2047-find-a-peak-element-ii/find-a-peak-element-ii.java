5c061d4d1bec5c1383f203e8fbcd3d32ee5670f8
kandulaakhila <akhilakandula735@gmail.com>
2026-01-09 22:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 116.4 MB (24.71%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,29 +1,31 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-       //better solution
-       //t.c:o(m.n)but early exit scan each row fully
+       //optimal solution
+       //t.c:finding max in col o(m) and b.s across col is o(logn)=o(mlogn);
        //s.c:o(1);
-       //Instead of scanning all cells blindly, scan row by row and pick the maximum in each 
-       // row. If that maximum is greater than its vertical neighbors, it’s a peak.
-      //This reduces comparisons but still O(m·n).
-      int m = mat.length, n = mat[0].length;
-        for (int i = 0; i < m; i++) {
-            // Find max element in row i
-            int col = 0;
-            for (int j = 1; j < n; j++) {
-                if (mat[i][j] > mat[i][col]) {
-                    col = j;
-                }
-            }
-
-            int val = mat[i][col];
-            int up   = (i > 0) ? mat[i-1][col] : -1;
-            int down = (i < m-1) ? mat[i+1][col] : -1;
-
-            if (val > up && val > down) {
-                return new int[]{i, col};
+       int m=mat.length;
+       int n=mat[0].length;
+       int left=0,right=n-1;
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        //find row index of max element in column mid
+        int row=0;
+        for(int i=0;i<m;i++){
+            if(mat[i][mid]>mat[row][mid]){
+                row=i;
             }
         }
-        return new int[]{-1, -1};
+        int midval=mat[row][mid];
+        int leftval=(mid>0)?mat[row][mid-1]:-1;
+        int rightval=(mid<n-1)?mat[row][mid+1]:-1;
+        if(midval>leftval && midval>rightval){
+            return new int[]{row,mid};//peak found;
+        }else if(leftval>midval){
+            right=mid-1;
+        }else{
+            left=mid+1;
+        }
+       }
+       return new int[]{-1,-1};//should never reach here
     }
 }
