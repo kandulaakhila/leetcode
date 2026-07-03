07b6c638c1ba8b3178b9036b97a3bbaf4e3b93bb
kandulaakhila <akhilakandula735@gmail.com>
2026-07-03 22:33:24 +0530

    Time: 4 ms (5.66%) | Memory: 46.6 MB (62.30%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //Brute force solution
+        LinkedHashSet<Integer>set= new LinkedHashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int index=0;
+        for(int num:set){
+        nums[index++]=num;
+        }
+        return set.size();
+    }
+}
