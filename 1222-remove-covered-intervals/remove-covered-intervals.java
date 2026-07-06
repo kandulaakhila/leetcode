f1b3d029a002c7f108e3965e24528534c6b92bf7
kandulaakhila <akhilakandula735@gmail.com>
2026-07-07 00:25:55 +0530

    Time: 6 ms (93.21%) | Memory: 46.7 MB (24.23%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,34 +1,20 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        //brute force solution
-         int n = intervals.length;
-
-        boolean[] covered = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = 0; j < n; j++) {
-
-                if (i == j)
-                    continue;
-
-                if (intervals[j][0] <= intervals[i][0] &&
-                    intervals[i][1] <= intervals[j][1]) {
-
-                    covered[i] = true;
-                    break;
-                }
+        //Optimal solution
+        Arrays.sort(intervals, (a,b)->{
+           if(a[0]==b[0]) {
+            return b[1]-a[1];
+           }
+           return a[0]-b[0];
+        });
+        int count=0;
+        int maxend=0;
+        for(int[] interval:intervals){
+            if(interval[1]>maxend){
+                count++;
+                maxend=interval[1];
             }
         }
-
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if (!covered[i])
-                count++;
-        }
-
         return count;
     }
 }
